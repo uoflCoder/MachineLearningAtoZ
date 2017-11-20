bf4a48d0f20ae5ec73fc14bb6566aaa9ed4abaab
uoflCoder <tylorcornett@gmail.com>
2017-11-20 16:14:42 -0500

    Checkpoint in tutorial series

--- Data_Preprocessing/missing_data.py
+++ Data_Preprocessing/missing_data.py
@@ -15,3 +15,18 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+label_encoder_X = LabelEncoder()
+X[:, 0] = label_encoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+
+
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state = 0)
--- Linear_Regression/Simple_Linear_Regression/tutorial.py
+++ Linear_Regression/Simple_Linear_Regression/tutorial.py
@@ -5,15 +5,40 @@ Created on Mon Nov 20 11:24:54 2017
 @author: stco224
 """
 
+#Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#import dataset X and y
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,1].values
 
+
+#Break the data into train and test sets
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state = 0)
 
+#Fit the data using simple linear regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
+#Get predicted values and the score
+y_pred = regressor.predict(X_test)
+
+#Visualize train set
+plt.scatter(X_train, y_train, color= 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title("Salary vs Experience (Training Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
+plt.show()
+
+#Visualize test set
+plt.scatter(X_test, y_test, color= 'red')
+plt.plot(X_test, regressor.predict(X_test), color = 'blue')
+plt.title("Salary vs Experience (Test Set)")
+plt.xlabel("Years of Experience")
+plt.ylabel("Salary")
